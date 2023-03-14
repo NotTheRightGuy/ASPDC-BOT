8fe5230eb3db524e98227eabc1af7c6ecb7dd63e
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-03-15 03:17:19 +0530

    Gonna be self hosting I guess

--- src/scripts/leetcodeScrapper.js
+++ src/scripts/leetcodeScrapper.js
@@ -1,8 +1,26 @@
-const request = require("request");
-let url = "https://leetcode.com/problemset/all/";
+const axios = require("axios");
+const cheerio = require("cheerio");
+const json = require("json");
 
-request(url, function (error, response, html) {
-    if (error) console.log(error);
-    console.log(response.statusCode);
-    console.log(html);
-});
+const url = "https://leetcode.com/problemset/all/";
+
+axios
+    .get(url, { withCredentials: true })
+    .then((response) => {
+        const pageHTML = response.data;
+        const $ = cheerio.load(pageHTML);
+        const scriptsList = $("script").last().html();
+        const scriptJSON = JSON.parse(scriptsList);
+        const POTD =
+            scriptJSON.props.pageProps.dehydratedState.queries[2].state.data.dailyCodingChallengeV2.challenges.slice(
+                -1
+            )[0];
+        const date = POTD.date;
+        const link = POTD.link;
+        const title = POTD.question.title;
+        const ID = POTD.question.questionFrontendId;
+        console.log(`${date}\n${link}\n${ID}\n${title}`);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
